87f5cbc1548089fa4aaa3e14bd230667fcaca331
YassaEmad <yassaemad86@gmail.com>
2025-09-03 21:01:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,49 +2,56 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
-
+// message functions
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
-
+// score functions
 const displayScore = function (score) {
   document.querySelector(".score").textContent = score;
 };
-
+//check btn
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-
+  // not a number
   if (!guess) {
     displayMessage("⛔️ No number!");
   }
   // =============================================================
   else if (guess === secretNumber) {
+    // if it's right
     displayMessage("🎉 Correct Number!");
+    // changing body color and style box
     document.querySelector("body").style.backgroundColor = "var(--color_secbg)";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
 
     if (score > highScore) {
       highScore = score;
+      //display the highest score
       document.querySelector(".Highscore").textContent = highScore;
     }
   }
   // ==================================================================
   else if (guess !== secretNumber) {
+    // if the guessing number is not equal to the random number
     if (score > 1) {
+      // display hint
       displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      // score decrement
       score--;
       displayScore(score);
     }
     // =============================================================
     else {
+      //loss
       displayMessage("💥 You lost the game!");
       displayScore(0);
     }
   }
   // =============================================================
 });
-
+//resetting
 document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
